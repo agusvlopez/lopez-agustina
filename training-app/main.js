b9cbcf7b4e384b3636e827dd900ba4be7711b455
agusvlopez <agusvlopez@gmail.com>
2023-10-06 19:44:10 -0300

    add: tailwind and chat style

--- training-app/main.js
+++ training-app/main.js
@@ -1,40 +1,5 @@
-// import { chatSaveMessage, chatSubscribeToMessages } from "./src/services/chat.js";
-
-// const salida = document.getElementById('salida');
-// const formChat = document.getElementById('form-chat');
-// const user = document.getElementById('user');
-// const message = document.getElementById('message');
-
-//        formChat.addEventListener('submit', function(e){
-//            e.preventDefault();
-//            const data = {
-//                user: user.value,
-//                message: message.value,
-//            }
-
-//            chatSaveMessage(data)
-//            .then(() => {
-//                 user.value = '',
-//                 message.value = ''
-//            });
-
-       
-//        });
-
-
-
-//  // messages = [{user: '', message: ''}]
-//  chatSubscribeToMessages(messages => {
-//      salida.innerHTML = messages.map(message => `<div>
-//          <div><b>Usuario: </b> ${message.user}</div>
-//           <div><b>Dijo: </b> ${message.message}</div>
-//       </div>`).join('');
-//   });
-
-
-
 //Creamos la app de vue
-
+import './src/main.css';
 import { createApp } from "vue";
 import App from "./src/App.vue";
 
--- training-app/src/pages/Chat.vue
+++ training-app/src/pages/Chat.vue
@@ -33,44 +33,54 @@ export default {
 
 </script>
 
-<template>
+<template> 
+    <h1 class="text-2xl bg-gray-900 text-white p-3">Chat</h1>
 
-    <div class="container">
-        <h1 class="m-2">Chat</h1>
-        <p>Leyendo los mensajes del chat en tiempo real</p>
+    <div class="container p-3">
+        
+        <p class="mb-4">¿Dudas o consultas? Chateá con nosotros y podemos ayudarte.</p>
+
+        <div>
+            <div v-for="message in messages">
+                <div><b>Usuario:</b> {{ message.user }}</div>
+                <div><b>Mensaje:</b> {{ message.message }}</div>
+            </div>
+        </div>
     
         <form 
         action="#" 
         @submit.prevent="sendMessage"
         id="form-chat" 
         class="col-8">
-            <div class="mb-3">
-                <label for="user" class="form-label">Usuario</label>
-                <input 
-                type="text" 
-                id="user" 
-                class="form-control"
-                v-model="newMessage.user"
-                >
+        
+            <div class="mt-6">
+                <div class="flex space-x-4">
+                    <label for="user" class="text-sm block font-bold">Usuario</label>
+                    <div >
+                   
+                        <input 
+                        type="text" 
+                        id="user" 
+                        class="mb-3 border-b-2 border-gray-500 px-3 focus:outline-none focus:border-indigo-600 flex-grow" 
+                        v-model="newMessage.user"
+                        >
+                    
+                    </div>
+                </div>
             </div>
-    
-            <div class="mb-3">
-                <label for="message" class="form-label">Mensaje</label>
-                <textarea 
-                id="message" 
-                cols="30" rows="6" 
-                class="form-control"
-                v-model="newMessage.message"></textarea>
+            <div class="mb-3 mt-3">
+                <label for="message"></label>
+                <div class="mt-2 flex">
+                    <textarea 
+                    id="message" 
+                    rows="1" 
+                    class="w-full border border-gray-300 p-2 focus:outline-none focus:border-blue-500" placeholder="Escribe tu mensaje..."
+                    v-model="newMessage.message"></textarea> 
+                    <button type="submit" class="bg-indigo-500 text-white px-4 py-2 hover:bg-indigo-600 focus:outline-none">Enviar</button>
+                </div>
             </div>
-            <button type="submit" class="btn btn-primary mb-3">Enviar</button>
+           
         </form>
-    
-        <div>
-            <div v-for="message in messages">
-                <div><b>Usuario: {{ message.user }}</b></div>
-                <div><b>Mensaje: {{ message.message }}</b></div>
-            </div>
-        </div>
 
     </div>
 
