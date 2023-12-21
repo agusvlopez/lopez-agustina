cdd895039df23ad86f4b1b56cc232a388f353feb
agusvlopez <agusvlopez@gmail.com>
2023-12-21 18:13:32 -0300

    refactor: indentation

--- training-app/src/App.vue
+++ training-app/src/App.vue
@@ -50,132 +50,111 @@ provide(notificationKey, {
 });
 
 const handleLogout = () => {
-  logout();
-  router.push('/iniciar-sesion');
-  
+    logout();
+    router.push('/iniciar-sesion');
+
 };
 
 </script>
 
 <template>
-<header>
-    <nav class="bg-black shadow-lg">
-    <div class="container mx-auto px-4">
-      <div class="flex justify-between items-center py-4">
-        <router-link to="/"><img src="./imgs/logo-blanco.png" alt="Logo Training App" class="maxWidth"></router-link>
-        <div class="hidden md:flex space-x-4">
-            <ul class="flex gap-4 text-white">
-                <li>
-                    <router-link to="/">Home</router-link>
-                </li>
-                <li>
-                    <router-link to="/precios">Precios de los planes</router-link>
-                </li>
+    <header>
+        <nav class="bg-black shadow-lg">
+            <div class="container mx-auto px-4">
+                <div class="flex justify-between items-center py-4">
+                    <router-link to="/"><img src="./imgs/logo-blanco.png" alt="Logo Training App"
+                            class="maxWidth"></router-link>
+                    <div class="hidden md:flex space-x-4">
+                        <ul class="flex gap-4 text-white">
+                            <li>
+                                <router-link to="/">Home</router-link>
+                            </li>
+                            <li>
+                                <router-link to="/precios">Precios de los planes</router-link>
+                            </li>
 
-            <template
-            v-if="user.id == null"
-            >
-                <li>
-                    <router-link to="/registro">Registrarse</router-link>
-                </li>
-                <li>
-                    <router-link to="/iniciar-sesion">Iniciar sesión</router-link>
-                </li>
-            </template>
+                            <template v-if="user.id == null">
+                                <li>
+                                    <router-link to="/registro">Registrarse</router-link>
+                                </li>
+                                <li>
+                                    <router-link to="/iniciar-sesion">Iniciar sesión</router-link>
+                                </li>
+                            </template>
 
-            <template
-            v-else
-            >
-            <!-- <template
-            v-if="user.rol === 'cliente' && user.fullProfileLoaded">
-                <li>
-                    <router-link to="/usuario/d6dfuuXe7laEyCh33M0uxKtb9xk1/chat">Chateá con nosotros</router-link>
-                </li>
-            </template> -->
-                <li>
-                    <router-link to="/perfil/">Mi perfil</router-link>
-                </li>
-                <li>
-                <form action=""
-                @submit.prevent="handleLogout">
-                    <button type="submit">{{user.email}} (Cerrar sesión)</button>
-                </form>
-                </li>
-            </template>
-            <template
-             v-if="user.rol === 'admin' && user.fullProfileLoaded" 
-            >
-                <li>
-                    <router-link
-                    to="/panel-admin">Panel Admin</router-link>
-                </li>
-            </template>
-            <template v-if="user.id !== null &&!user.fullProfileLoaded">
-                <Loader></Loader>
-            </template>
-            </ul>
+                            <template v-else>
+                                <li>
+                                    <router-link to="/perfil/">Mi perfil</router-link>
+                                </li>
+                                <li>
+                                    <form action="" @submit.prevent="handleLogout">
+                                        <button type="submit">{{ user.email }} (Cerrar sesión)</button>
+                                    </form>
+                                </li>
+                            </template>
+                            <template v-if="user.rol === 'admin' && user.fullProfileLoaded">
+                                <li>
+                                    <router-link to="/panel-admin">Panel Admin</router-link>
+                                </li>
+                            </template>
+                            <template v-if="user.id !== null && !user.fullProfileLoaded">
+                                <Loader></Loader>
+                            </template>
+                        </ul>
 
+                    </div>
+                    <div class="md:hidden flex items-center">
+                        <button @click="mobileMenuOpen = !mobileMenuOpen" class="text-white">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M4 6h16M4 12h16M4 18h16" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div v-if="mobileMenuOpen" class="md:hidden">
+                <router-link to="/" class="block text-white p-3 hover:bg-gray-600">Home</router-link>
+                <router-link to="/precios" class="block text-white p-3 hover:bg-gray-600">Precios de planes</router-link>
+                <template v-if="user.id == null">
+                    <router-link to="/registro" class="block text-white p-3 hover:bg-gray-600">Registrarse</router-link>
+                    <router-link to="/iniciar-sesion" class="block text-white p-2 hover:bg-gray-600">Iniciar
+                        sesión</router-link>
+                </template>
+                <template v-else>
+                    <template v-if="user.rol === 'cliente'">
+                        <router-link to="/usuario/d6dfuuXe7laEyCh33M0uxKtb9xk1/chat"
+                            class="block text-white p-3 hover:bg-gray-600">Chateá con nosotros</router-link>
+                    </template>
+                    <router-link to="/perfil" class="block text-white p-3 hover:bg-gray-600">Mi perfil</router-link>
+                    <form class="block text-white p-3 hover:bg-gray-600" action="" @submit.prevent="handleLogout">
+                        <button type="submit">{{ user.email }} (Cerrar sesión)</button>
+                    </form>
+                </template>
+                <template v-if="user.rol === 'admin' && user.fullProfileLoaded">
+                    <router-link to="/panel-admin" class="block text-white p-3 hover:bg-gray-600">Panel Admin</router-link>
+                </template>
+                <template v-if="!user.fullProfileLoaded">
+                    <Loader></Loader>
+                </template>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div class="container h-full m-auto p-4">
+            <div v-if="notification">
+                <Notification :message="notification.message" :type="notification.type" />
+            </div>
+            <router-view></router-view>
         </div>
-        <div class="md:hidden flex items-center">
-          <button @click="mobileMenuOpen = !mobileMenuOpen" class="text-white">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-            </svg>
-          </button>
+        <div v-if="user.fullProfileLoaded && user.rol === 'cliente'"
+            class="chat-link bg-white p-4 rounded-full shadow-lg transition-transform transform hover:-translate-y-2 hover:shadow-md">
+            <router-link to="/usuario/d6dfuuXe7laEyCh33M0uxKtb9xk1/chat">
+                <img :src="chatIcon" alt="Chat" class="chat-icon">
+            </router-link>
         </div>
-      </div>
-    </div>
-    <div v-if="mobileMenuOpen" class="md:hidden">
-      <router-link to="/" class="block text-white p-3 hover:bg-gray-600">Home</router-link>
-      <router-link to="/precios" class="block text-white p-3 hover:bg-gray-600">Precios de planes</router-link>
-      <template
-        v-if="user.id == null"
-        >
-        <router-link to="/registro" class="block text-white p-3 hover:bg-gray-600">Registrarse</router-link>
-        <router-link to="/iniciar-sesion" class="block text-white p-2 hover:bg-gray-600">Iniciar sesión</router-link>
-      </template>
-        <template
-        v-else
-        >
-        <template
-            v-if="user.rol === 'cliente'">
-            <router-link to="/usuario/d6dfuuXe7laEyCh33M0uxKtb9xk1/chat" class="block text-white p-3 hover:bg-gray-600">Chateá con nosotros</router-link>
-        </template>
-            <router-link to="/perfil" class="block text-white p-3 hover:bg-gray-600">Mi perfil</router-link>
-             <form 
-             class="block text-white p-3 hover:bg-gray-600"
-             action=""
-                @submit.prevent="handleLogout">
-                    <button type="submit">{{user.email}} (Cerrar sesión)</button>
-                </form>
-        </template>
-        <template
-        v-if="user.rol === 'admin' && user.fullProfileLoaded" 
-        >
-            <router-link to="/panel-admin" class="block text-white p-3 hover:bg-gray-600">Panel Admin</router-link>
-        </template>
-        <template v-if="!user.fullProfileLoaded">
-                <Loader></Loader>
-        </template>
-    </div>
-  </nav>
-</header>
-<main>
-    <div class="container h-full m-auto p-4">
-        <div v-if="notification">
-            <Notification :message="notification.message" :type="notification.type" />
-        </div>
-        <router-view></router-view>
-    </div>
-    <div
-        v-if="user.fullProfileLoaded && user.rol === 'cliente'"
-        class="chat-link bg-white p-4 rounded-full shadow-lg transition-transform transform hover:-translate-y-2 hover:shadow-md"
-    >
-        <router-link to="/usuario/d6dfuuXe7laEyCh33M0uxKtb9xk1/chat">
-            <img :src="chatIcon" alt="Chat" class="chat-icon">
-        </router-link>
-    </div>
-</main>
+    </main>
     <footer class="flex justify-center items-center h-[100px] bgNav text-white">
         <p>Training App &copy; 2023</p>
     </footer>
--- training-app/src/pages/Register.vue
+++ training-app/src/pages/Register.vue
@@ -5,12 +5,10 @@ import RegisterForm from '../components/RegisterForm.vue';
 </script>
 
 <template>
-<div class="min-h-screen flex items-center justify-center bg-gray-100">
-    <div class="bg-white p-8 rounded-lg shadow-md w-96">
-      <BaseH1>Crear cuenta</BaseH1>
-      <RegisterForm />
+    <div class="min-h-screen flex items-center justify-center bg-gray-100">
+        <div class="bg-white p-8 rounded-lg shadow-md w-96">
+            <BaseH1>Crear cuenta</BaseH1>
+            <RegisterForm />
+        </div>
     </div>
-</div>
 </template>
-
-
